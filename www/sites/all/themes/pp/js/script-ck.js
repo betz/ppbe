91c900482af30e473989baadef7c9e3bf3ca2dc7
tom behets <behets@gmail.com>
2012-12-01 08:39:11 +0100

    blog posts comments; extra profile information

---
+++ www/sites/all/themes/pp/js/script-ck.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function(e){e(function(){e("#comments .intended").masonry({itemSelector:".comment",columnWidth:180})})})(jQuery);
